9d859943eb5bfdde104c8b58ab8da6b2e8766cc4
Igor Torgaiev <Itorgaiev@gmail.com>
2020-09-30 19:02:50 +0200

    PHP-Syntax: additional example

---
+++ text.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Introduction into PHP</h1>
+
+<?php
+echo "Hello World!<br>";
+echo "<hr>";
+?>
+
+<?php
+echo "Let's study <em><strong>PHP</strong></em><br><br>";
+?>
+
+<?php
+$hobby = "programming";
+$language = "PHP";
+echo "My name is Igor.<br>";
+echo "I am interested in " .$hobby.".<br>";
+?>
+
+
+</body>
+</html>
